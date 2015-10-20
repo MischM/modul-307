6211f9c2296d887c899c1ba87acfca2c5292e48f
Tobias Kündig <tobias.kuendig@offlinegmbh.ch>
2015-10-20 15:20:01 +0200

    Code korrigiert

--- Tag 1/04 PHP Aufgaben/01 Bier/src/beers_2.php
+++ Tag 1/04 PHP Aufgaben/01 Bier/src/beers_2.php
@@ -2,9 +2,11 @@
 for ($beers = 99; $beers > 0; $beers--) {
 
     $oneLess = $beers - 1;
-    $bottles = $beers === 1 ? 'bottle' : 'bottles';
+
+    $bottles        = $beers   === 1 ? 'bottle' : 'bottles';
+    $bottlesOneLess = $oneLess === 1 ? 'bottle' : 'bottles';
 
     echo "${beers} ${bottles} of beer on the wall, ${beers} ${bottles} of beer.\n"
-       . "Take one down and pass it around, ${oneLess} bottles of beer on the wall.\n\n";
+       . "Take one down and pass it around, ${oneLess} ${bottlesOneLess} of beer on the wall.\n\n";
 
 }
--- Tag 1/04 PHP Aufgaben/01 Bier/src/beers_3.php
+++ Tag 1/04 PHP Aufgaben/01 Bier/src/beers_3.php
@@ -2,11 +2,13 @@
 for ($beers = 99; $beers >= 0; $beers--) {
 
     $oneLess = $beers - 1;
-    $bottles = $beers === 1 ? 'bottle' : 'bottles';
+
+    $bottles        = $beers   === 1 ? 'bottle' : 'bottles';
+    $bottlesOneLess = $oneLess === 1 ? 'bottle' : 'bottles';
 
     if ($beers > 0) {
         echo "${beers} ${bottles} of beer on the wall, ${beers} ${bottles} of beer.\n"
-           . "Take one down and pass it around, ${oneLess} bottles of beer on the wall.\n\n";
+           . "Take one down and pass it around, ${oneLess} ${bottlesOneLess} of beer on the wall.\n\n";
     } else {
         echo "No more bottles of beer on the wall, no more bottles of beer,\n"
            . "Go to the store and buy some more, 99 bottles of beer on the wall.\n\n";
