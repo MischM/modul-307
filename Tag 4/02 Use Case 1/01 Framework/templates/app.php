14687c14523bf3c3e033ef55cf8d44ff9d5f9ddc
Tobias Kündig <tobias.kuendig@offlinegmbh.ch>
2016-01-01 17:59:10 +0100

    Templates für css und js erstellt

--- Tag 4/02 Use Case 1/01 Framework/templates/app.php
+++ Tag 4/02 Use Case 1/01 Framework/templates/app.php
@@ -3,8 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Template</title>
+
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
+    
     <?php include view($view); ?>
+
+    <script src="http://code.jquery.com/jquery.min.js"></script>
+    <script src="assets/app.js"></script>
 </body>
 </html>
